c15c1e1c9e74e6b67ec0023413b99e1706daffd1
Pamparampam <you@example.com>
2026-03-14 13:49:03 +0100

    Added support for multiple games

--- cw1/Program.cs
+++ cw1/Program.cs
@@ -1,28 +1,41 @@
-﻿
+﻿using System;
+
 class Program
 {
     static void Main()
     {
         Random rand = new Random();
 
-        Console.WriteLine("Papier Kamień Nożyce");
-        Console.WriteLine("1 - Kamień");
-        Console.WriteLine("2 - Papier");
-        Console.WriteLine("3 - Nożyce");
-        Console.Write("Twój wybór: ");
+        while (true)
+        {
+            Console.WriteLine("Papier Kamień Nożyce");
+            Console.WriteLine("1 - Kamień");
+            Console.WriteLine("2 - Papier");
+            Console.WriteLine("3 - Nożyce");
+            Console.WriteLine("0 - Wyjście");
+            Console.Write("Twój wybór: ");
 
-        int player = int.Parse(Console.ReadLine());
-        int computer = rand.Next(1, 4);
+            int player = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Komputer: " + computer);
+            if (player == 0)
+                break;
 
-        if (player == computer)
-            Console.WriteLine("Remis");
-        else if ((player == 1 && computer == 3) ||
-                 (player == 2 && computer == 1) ||
-                 (player == 3 && computer == 2))
-            Console.WriteLine("Wygrywasz!");
-        else
-            Console.WriteLine("Przegrywasz!");
+            int computer = rand.Next(1, 4);
+
+            Console.WriteLine("Komputer: " + computer);
+
+            if (player == computer)
+                Console.WriteLine("Remis");
+            else if ((player == 1 && computer == 3) ||
+                     (player == 2 && computer == 1) ||
+                     (player == 3 && computer == 2))
+                Console.WriteLine("Wygrywasz!");
+            else
+                Console.WriteLine("Przegrywasz!");
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Koniec gry");
     }
 }
